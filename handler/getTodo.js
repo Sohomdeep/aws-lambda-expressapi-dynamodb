c71d9a9b203616c88de0f449581958100a39f8dc
Sohomdeep <sohomdeeppaul@gmail.com>
2022-12-01 17:11:38 +0530

    get todo api

---
+++ handler/getTodo.js
@@ -0,0 +1,32 @@
+const AWS = require("aws-sdk");
+
+const TODO_TABLE = process.env.TODO_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const uuid = require("uuid");
+
+module.exports.listTodos = (event, context, callback) => {
+    const params = {
+        TableName: TODO_TABLE,
+        key: {
+            id: event.pathParameters.id
+        }
+    }
+
+    dynamoDb.get(params, (error, data) => {
+        if(error){
+            console.error(error);
+            callback(new Error(error));
+            return;
+        }
+        //response
+        const response = data.Item ? {
+            statusCode: 200,
+            body: JSON.stringify(data.Items)
+        } : {
+            statusCode: 404,
+            body: JSON.stringify({message: "Todo not found"})
+        }
+        callback(null, response);
+    });
+
+}
